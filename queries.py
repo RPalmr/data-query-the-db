e32c271207d3ea55589a4e0b59c0f8da2975611e
reece palmer <reecelpalmer@gmail.com>
2023-10-20 11:45:32 +0100

    Progress on 8 tests passing

--- queries.py
+++ queries.py
@@ -2,19 +2,32 @@
 
 def query_orders(db):
     # return a list of orders displaying each column
-    query = "SELECT * FROM ORDERS ORDER BY OrderID ASC;"
-    db.execute(query)
-    orders = [tuple(row) for row in db.fetchall()]
-    
-    return orders
+    sql_query = "SELECT * FROM orders ORDER BY OrderID ASC"
+    results = db.execute(sql_query)
+    results = results.fetchall()
+    return results
+
 
 def get_orders_range(db, date_from, date_to):
     # return a list of orders displaying all columns with OrderDate between
     # date_from and date_to (excluding date_from and including date_to)
-    pass  # YOUR CODE HERE
+    sql_query = "SELECT * FROM orders WHERE OrderDate > ? AND OrderDate <= ? ORDER BY OrderDate ASC"
+    results = db.execute(sql_query, (date_from, date_to))
+    results = results.fetchall()
+
+    return results
 
 def get_waiting_time(db):
     # get a list with all the orders displaying each column
     # and calculate an extra TimeDelta column displaying the number of days
     # between OrderDate and ShippedDate, ordered by ascending TimeDelta
-    pass  # YOUR CODE HERE
+    sql_query = """
+    SELECT *, (ShippedDate - OrderDate) AS DeliveryTime
+    FROM orders
+    WHERE ShippedDate IS NOT NULL
+    ORDER BY DeliveryTime ASC
+    """
+    results = db.execute(sql_query)
+    results = results.fetchall()
+
+    return results
