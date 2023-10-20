df94acd8329668f8b48f486222c23d84f2f4c0f3
reece palmer <reecelpalmer@gmail.com>
2023-10-20 14:13:35 +0100

    Update typo

--- queries.py
+++ queries.py
@@ -2,8 +2,8 @@
 
 def query_orders(db):
     # return a list of orders displaying each column
-    sql_query = "SELECT * FROM orders ORDER BY OrderID ASC"
-    results = db.execute(sql_query)
+    query = "SELECT * FROM orders ORDER BY OrderID ASC"
+    results = db.execute(query)
     results = results.fetchall()
     return results
 
@@ -11,8 +11,8 @@ def query_orders(db):
 def get_orders_range(db, date_from, date_to):
     # return a list of orders displaying all columns with OrderDate between
     # date_from and date_to (excluding date_from and including date_to)
-    sql_query = "SELECT * FROM orders WHERE OrderDate > ? AND OrderDate <= ? ORDER BY OrderDate ASC"
-    results = db.execute(sql_query, (date_from, date_to))
+    query = "SELECT * FROM orders WHERE OrderDate > ? AND OrderDate <= ? ORDER BY OrderDate ASC"
+    results = db.execute(query, (date_from, date_to))
     results = results.fetchall()
 
     return results
@@ -21,13 +21,13 @@ def get_waiting_time(db):
     # get a list with all the orders displaying each column
     # and calculate an extra TimeDelta column displaying the number of days
     # between OrderDate and ShippedDate, ordered by ascending TimeDelta
-    sql_query = """
+    query = """
     SELECT *, (JULIANDAY(ShippedDate) - JULIANDAY(OrderDate)) AS JulianTime
     FROM orders
     WHERE ShippedDate IS NOT NULL
     ORDER BY JulianTime ASC
     """
-    results = db.execute(sql_query)
+    results = db.execute(query)
     results = results.fetchall()
 
     return results
